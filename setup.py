e7b71b31ec0fe451f457fc2bb72f8d51540651fc
Lazizjonov <44605450+Lazizjonov@users.noreply.github.com>
2022-12-09 18:13:14 +0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,14 @@
 from setuptools import setup
+
 setup(
-    name='app-name'
+    name='qr-bot',
     version='1.0',
-    author='Bejamin Frakline',
-    description='A brief synopsis of the project',
-    long_description='A much longer explanation of the project and helpful resources',
-    url='https://github.com/BenjaminFranline',
+    author='Lazizjonov Jasurbek',
+    description='qr-code scanning tg-bot',
+    long_description='Pharmiq.uz telegram bot for scanning qr-codes with products',
+    url='https://github.com/Laboratory13/telegram-bot-qr-code-scanner',
     keywords='development, setup, setuptools',
     python_requires='>=3.8, <4',
-    install_requires=[
-        'PyYAML',
-        'pandas==0.23.3',
-        'numpy>=1.14.5',
-        'matplotlib>=2.2.0,,
-        'jupyter'
-    ]
+    install_requires=[ 'aiogram' ]
 )
+
