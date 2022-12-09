2d5769a6428bfc375c22d21f406d2d119d745351
Lazizjonov <44605450+Lazizjonov@users.noreply.github.com>
2022-12-09 20:57:20 +0500

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,9 @@
 from helpers.filter import Is_admin
 from aiogram import Bot, Dispatcher
 
-API_TOKEN = '5867243559:AAE9FtL9Hxrpy2geAsWNLB70Zz__CbJB-EM' # Bositkhon's bot
+# API_TOKEN = '5867243559:AAE9FtL9Hxrpy2geAsWNLB70Zz__CbJB-EM' # Bositkhon's bot
 # API_TOKEN = '5869984480:AAFtmMayAt7qdkWGCtaNyovORNKT0P1h0zQ'
-# API_TOKEN = '477383024:AAE33xFWSas6jRROncgrsVOacJrrsDtHCkI'
+API_TOKEN = '477383024:AAE33xFWSas6jRROncgrsVOacJrrsDtHCkI'
 
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
